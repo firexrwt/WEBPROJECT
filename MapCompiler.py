ea9b19552c5d336f8c3ef2a873505eb38f9f34c4
firex <stepanveremeev@gmail.com>
2021-04-21 17:55:09 +0300

    добавил переменные с обращением к Алисе

--- MapCompiler.py
+++ MapCompiler.py
@@ -1,4 +1,9 @@
+import json
+
+from flask import Flask, request
 import requests
+
+app = Flask(__name__)
 
 
 def Compile_Map(name):
@@ -18,3 +23,37 @@ def Compile_Map(name):
 
     req2 = f'https://static-maps.yandex.ru/1.x/?ll={c[0]},{c[1]}&spn={area ** 0.5},{area ** 0.5}&l=map'
     return requests.get(req2)
+
+
+@app.route('\post', methods=['POST'])
+def get_alice_request():
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+
+    handle_dialog(request.json, response)
+    return json.dumps(response)
+
+
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+    if req['session']['new']:
+        res['response']['text'] = 'Привет, какую страну или город ты хочешь увидеть на карте?'
+        session_state['user_id'] = {
+            'state': 1
+        }
+        return
+    states[session_state[user_id]['state']](user_id, req, res)
+
+
+states = {
+    1: Compile_Map
+}
+session_state = {}
+
+if __name__ == '__main__':
+    app.run()
